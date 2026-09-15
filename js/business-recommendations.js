f73e3dc8d6778da1fd8b18cc045270b5c64aceaa
dhays4sports <108902589+dhays4sports@users.noreply.github.com>
2026-09-15 15:19:18 -0700

    Delete js directory

--- js/business-recommendations.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const engine = window.CoverageFitRecommendationEngine;
-  if (!engine) throw new Error('CoverageFitRecommendationEngine is required.');
-  window.CoverageFitBusinessRecommendations = Object.freeze({
-    generate(report) { return engine.generate('business', report); },
-    priorityLabel: engine.PRIORITY_LABEL,
-    engine
-  });
-})();
